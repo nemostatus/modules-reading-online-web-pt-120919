bd7e747dfec22362e923b49b1f94ebf150377a54
Matthew Corralejo <mattcorralejo1@gmail.com>
2020-01-23 08:32:13 +0000

    Automatically backed up by Learn

--- lib/fancy_dance.rb
+++ lib/fancy_dance.rb
@@ -1,20 +1,21 @@
 module FancyDance
   module InstanceMethods
-
-   
-
-    def jump
-      "Look how high I'm jumping!"
-    end
-
-    def pirouette
-      "I'm doing a pirouette"
-    end
-
-    def take_a_bow
-      "Thank you, thank you. It was a pleasure to dance for you all."
-    end
-  end
+def twirl 
+   "I'm twirling"
+ end 
+ 
+  def jump 
+    "Look how high I'm jumping!"
+  end 
+ 
+  def pirouette 
+    "I'm doing a piroutte"
+  end 
+ 
+  def take_a_bow
+    "Thank you,thank you.It's been a pleasure dancing for you all."
+  end 
+end 
 
   module ClassMethods
 
