7d1894a6e246e3b7906f4c761783aa7b206991db
Matthew Corralejo <mattcorralejo1@gmail.com>
2020-01-23 08:23:17 +0000

    Automatically backed up by Learn

--- lib/dance_module.rb
+++ lib/dance_module.rb
@@ -8,9 +8,8 @@ module Dance
   def jump 
     "Look how high I'm jumping!"
  
-  def pirouette
-    "I'm doing a pirouette"
-  end
+  def pirouette 
+    "I'm doing a piroutte"
  
   def take_a_bow
     "Thank you, thank you. It was a pleasure to dance for you all."
